d9b9b10c2f8ce0c23e5715a58e88e11fc9efffa7
DavidG0m3z <david.ardilagomez@gmail.com>
2026-03-09 16:44:18 -0500

    Featire: add AuthController

---
+++ src/TaskManager.Api/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Application.Common.DTOs.Auth.Commands.Login;
+using TaskManager.Application.Features.Auth.Commands.Login;
+using TaskManager.Application.Features.Auth.Commands.Register;
+using TaskManager.Application.Features.Auth.Commands.Registrer;
+
+namespace TaskManager.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            if (!result.IsFailure)
+            {
+                return Unauthorized(new { message = result.Error });
+
+            }
+
+            return Ok(result.Value);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody], RegisterCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            if (!result.IsFailure)
+            {
+                return BadRequest(new {message = result.Error});
+            }
+
+            return CreatedAtAction(nameof(Login), result.Value);
+        } 
+
+    }
+}
